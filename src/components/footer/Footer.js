da11b58c301b03a4f3e41a7432eaf79f2c3a00bd
ahmadhelwa <AHM0173377@ju.edu.jo>
2022-05-10 20:28:20 +0300

    task16

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,14 +6,36 @@ function Footer()
 
 
 return (
+<div class="footer">
+    <div class="section1-footer">
+            <p>
+                phone number:
+            </p>
+            <p>
+                +962785889613
+            </p>
+            <p>
+                Amman-jordan
+            </p>
+            <p>
+                <a href="">@copyright:Salehziad</a>
+            </p>
+        </div>
 
+        <div class="social-buttons">
+                    <a target="_blank" href="https://www.facebook.com/profile.php?id=100005955198338"><i
+                            class="fab fa-facebook"></i></a>
+                    <a target="blank" href="https://instagram.com/saleh.dalalshh?igshid=YmMyMTA2M2Y="><i
+                            class="fab fa-instagram"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/saleh-ziad-6b40a1214"><i
+                            class="fab fa-linkedin-in"></i></a>
+                    <a target="_blank" href="https://github.com/Salehziad"><i class="fab fa-github"></i></a>
+                </div>
 
-<div class="footer">  
 
 
 
 </div>
-
 
 )
 
